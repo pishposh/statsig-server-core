7585a9cafd8a4fd48c0bf80ced13830218c55b87
Kentaro Kaji <kentaro.kaji@nytimes.com>
2025-09-17 17:01:25 -0400

    restore statsig_ffi.go #cgo

--- statsig-go/src/statsig_ffi.go
+++ statsig-go/src/statsig_ffi.go
@@ -1,7 +1,8 @@
 package statsig
 
 /*
-// Note: link paths set via build.sh (CGO_CFLAGS/CGO_LDFLAGS) for wasm builds.
+#cgo LDFLAGS: -lstatsig_ffi
+
 #include "statsig_ffi.h"
 #include <stdlib.h>
 */
